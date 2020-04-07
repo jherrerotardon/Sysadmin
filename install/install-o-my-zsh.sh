020488a16ad154ea7700ad68586a0d2f43f4aa97
jherrerotardon <jherrero.tardon@gmail.com>
2020-04-08 01:33:13 +0200

    Fix install zsh script.

--- install/install-o-my-zsh.sh
+++ install/install-o-my-zsh.sh
@@ -1,35 +1,36 @@
-FILE_PATH=$(dirname $(realpath $0))
-FILES_PATH=$FILE_PATH/../files
-ZSHRC_TEMPLATE=$FILES_PATH/.zshrc
-ALIAS_FILE='$HOME/Projects/sysadmin/utils/alias/.self_profile'
+#!/bin/sh
+
+FILE_PATH=$(dirname "$(realpath "$0")")
+FILES_PATH="$FILE_PATH"/../files
+ZSHRC_TEMPLATE="$FILES_PATH"/.zshrc
+ALIAS_FILE="$HOME"/Projects/sysadmin/utils/alias/.self_profile
+ZSH='/opt/oh-my-zsh'
 
 # Install pre-requisites.
 sudo apt install git curl zsh -y
 
-# As ROOT
-sudo su
+##### Root install ######
 
 # Install OH MY ZSH in /opt/
-export ZSH="/opt/oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sh -c "export CHSH='no' RUNZSH='no' ; $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Spaceship prompt
-git clone "https://github.com/denysdovhan/spaceship-prompt.git" "$ZSH/custom/themes/spaceship-prompt" 
-ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
+sudo git clone "https://github.com/denysdovhan/spaceship-prompt.git" "$ZSH/custom/themes/spaceship-prompt" 
+sudo ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
 
 # Syntax highlight
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /opt/zsh-syntax-highlighting
-echo "source /opt/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /opt/zsh-syntax-highlighting
 
 # Change the shell of the root user
-chsh -s $(which zsh)
+sudo cp $ZSHRC_TEMPLATE /root/.zshrc
 
-exit
+# Change the shell of root.
+sudo chsh -s $(which zsh) root
+
+##### Finish Root install ######
 
 # Change the shell of your user
 chsh -s $(which zsh) $USER
-
-# Update the theme to spaceship and place the values:
-nano ~/.zshrc
 
 # Copy the zshrc to the user and check
 cp $ZSHRC_TEMPLATE $HOME/.zshrc
